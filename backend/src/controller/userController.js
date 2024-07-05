820af9ab9bc82cf46473cd74158d9b1976f72676
marcos <martinmarcospablo@gmail.com>
2024-07-04 21:05:10 -0300

    modificación useController

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -11,39 +11,49 @@ const getAllUsers = (req, res) => {
 }
 
 const getUserById = (req, res) => {
-    const {id} = req.params;
-    const sql = 'SELECT * FROM Usuario WHERE ID = ?';
+    const { id } = req.params;
+    const sql = 'SELECT * FROM Usuario WHERE id = ?';
     db.query(sql,[id], (err, result) => 
         {
             if(err) throw err; 
-            res.json(result);
+            res.json(result[0]);
         });
 
 }
 
 const createUser = (req, res) => {
-    const {nombre, apellido, mail, contraseña} = req.body;
+    const { nombre, apellido, email, contraseña, id_rol } = req.body;
 
-    const sql = 'INSERT INTO Usuario (nombre,apellido,mail,contraseña) VALUES (?,?,?,?)';
+    
+    if (!Number.isInteger(id_rol)) {
+        return res.status(400).json({ mensaje: 'El valor de id_rol debe ser un número entero válido' });
+    }
 
-    db.query(sql,[nombre, apellido, mail, contraseña], (err, result) => 
-        {
-            if(err) throw err;
+    
+    const sql = 'INSERT INTO Usuario (nombre,apellido,email,contraseña,id_rol) VALUES (?,?,?,?,?)';
 
-            res.json({
-                mensaje : 'Usuario Creado con Éxito',
-                idUsuario: result.insertId
-            });
-        })
+    
+    db.query(sql, [nombre, apellido, email, contraseña, id_rol], (err, result) => {
+        if (err) {
+            console.error('Error al crear usuario:', err);
+            return res.status(500).json({ mensaje: 'Error al crear usuario' });
+        }
 
-}
+        
+        res.json({
+            mensaje: 'Usuario Creado con Éxito',
+            idUsuario: result.insertId
+        });
+    });
+};
+
 
 const updateUser = (req, res) => {
     const {id} = req.params;
-    const {nombre,apellido,mail} = req.body;
+    const {nombre,apellido,email} = req.body;
 
-    const sql = 'UPDATE Usuario SET nombre = ?, apellido = ?, mail = ? WHERE id = ?'
-    db.query(sql,[nombre,apellido,mail,id], (err, result) => 
+    const sql = 'UPDATE Usuario SET nombre = ?, apellido = ?, email = ? WHERE id = ?'
+    db.query(sql,[nombre,apellido,email,id], (err, result) => 
     {
         if(err) throw err;
 
