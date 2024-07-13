52c1a5a718b4512fe5908a1ec65fa3d7f9d076ef
Nicolás Cartellone <65472865+NicoCartellone@users.noreply.github.com>
2024-07-12 21:14:54 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 const corsOptions = {
-    origin: 'https://cac-ecommerce.netlify.app/',
+    origin: 'https://cac-ecommerce.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
 };
